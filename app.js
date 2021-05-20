13598b72cb713f8b086fb7212af1b7bddb846365
scott-olson <scotteolson3@gmail.com>
2021-05-19 20:02:12 -0700

    add brew function

--- app.js
+++ app.js
@@ -18,14 +18,42 @@ const client = new tmi.Client({
 // instantiate the client connection
 client.connect();
 
-// generic welcoming message
+// currentBrew holds the current brew for the command !brew
+var currentBrew = 'Tremor California Lager';
+
+// monitor messages
 client.on('message', (channel, tags, message, self) => {
     // protect against loop on bot messages
     if (self) return;
 
+    // hello world 
     if (message.toLowerCase() === "!hello") {
         console.log(tags, self)
         client.say(channel, `@${tags.username}, Hi! berdroNCheers`);
     }
 
-})
+    // !brew returns the currently assigned brew 
+    // sets the currentBrew if mod adds too message
+    if (message.toLowerCase().startsWith('!brew')){
+        if (tags.mod && message.length > 5) {
+            // parse the message, take of the command and save the rest
+            var m = message.slice(5);
+            currentBrew = m;
+            currentBrewResponse(channel);
+        }
+        else{
+            currentBrewResponse(channel);
+        };
+    };
+
+    if (message.toLowerCase() === '!bot') {
+        client.say(channel, "Beep boop, I am a bot.");
+    }
+
+});
+
+
+function currentBrewResponse(channel) {
+    var mes = `Berdron is drinking ${currentBrew}. berdroNCheers berdroNCheers berdroNCheers`;
+    client.say(channel, mes);
+};
